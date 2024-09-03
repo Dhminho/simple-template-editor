018329812cee18f70caa6f3abb280f115e2eea31
Dhminho <62530061+Dhminho@users.noreply.github.com>
2024-09-03 05:33:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,15 @@ document.getElementById('image-upload').addEventListener('change', function(even
 });
 
 document.getElementById('download-button').addEventListener('click', function() {
+  const downloadButton = document.getElementById('download-button');
+  downloadButton.style.display = 'none'; // إخفاء زر التحميل مؤقتًا
+
   html2canvas(document.querySelector('#editor'), {
     backgroundColor: null, // تعيين الخلفية كـ null لجعلها شفافة
     useCORS: true // استخدام CORS للسماح بتحميل الصور من مصادر خارجية
   }).then(canvas => {
+    downloadButton.style.display = 'block'; // إعادة إظهار زر التحميل
+
     const link = document.createElement('a');
     link.href = canvas.toDataURL('image/png');
     link.download = 'template.png';
