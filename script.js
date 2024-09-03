dfefd81dee44a8df765063b5c22e42f4fc90f493
Dhminho <62530061+Dhminho@users.noreply.github.com>
2024-09-03 06:33:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ document.getElementById('image-upload').addEventListener('change', function(even
   }
 });
 
+document.getElementById('uploaded-image').addEventListener('click', function() {
+  document.getElementById('image-upload').click();
+});
+
 document.getElementById('text-rect').addEventListener('focus', function() {
   if (this.innerText === 'أدخل النص هنا') {
     this.innerText = '';
@@ -46,26 +50,4 @@ interact('#uploaded-image')
       const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
       const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
-      target.style.transform = `translate(${x}px, ${y}px)`;
-      target.setAttribute('data-x', x);
-      target.setAttribute('data-y', y);
-    }
-  })
-  .resizable({
-    edges: { left: true, right: true, bottom: true, top: true }
-  })
-  .on('resizemove', function(event) {
-    const target = event.target;
-    let x = (parseFloat(target.getAttribute('data-x')) || 0);
-    let y = (parseFloat(target.getAttribute('data-y')) || 0);
-
-    target.style.width = `${event.rect.width}px`;
-    target.style.height = `${event.rect.height}px`;
-
-    x += event.deltaRect.left;
-    y += event.deltaRect.top;
-
-    target.style.transform = `translate(${x}px, ${y}px)`;
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
-  });
+      target.style.transform = `translate(${x}px, ${y}px)
