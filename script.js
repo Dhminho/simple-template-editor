e00efe6bc3b675ccf3b9e46a934ac27a0b31055c
Dhminho <62530061+Dhminho@users.noreply.github.com>
2024-09-03 06:48:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -50,4 +50,26 @@ interact('#uploaded-image')
       const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
       const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
-      target.style.transform = `translate(${x}px, ${y}px)
+      target.style.transform = `translate(${x}px, ${y}px)`;
+      target.setAttribute('data-x', x);
+      target.setAttribute('data-y', y);
+    }
+  })
+  .resizable({
+    edges: { left: true, right: true, bottom: true, top: true }
+  })
+  .on('resizemove', function(event) {
+    const target = event.target;
+    let x = (parseFloat(target.getAttribute('data-x')) || 0);
+    let y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+    target.style.width = `${event.rect.width}px`;
+    target.style.height = `${event.rect.height}px`;
+
+    x += event.deltaRect.left;
+    y += event.deltaRect.top;
+
+    target.style.transform = `translate(${x}px, ${y}px)`;
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  });
